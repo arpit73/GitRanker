9f7f81d845eb52156370c30a047032bfdd3f6575
Arpit <rpittrer@gmail.com>
2018-09-11 02:59:15 +0530

    s

--- Authors/getAuthor.js
+++ Authors/getAuthor.js
@@ -1,38 +1,20 @@
 const rp = require('request-promise');
 
 var options = {
-    uri: `https://api.github.com/users/arpit73`,
+    uri: `https://api.github.com/users/arpit73/repos`,
+    headers: {
+        'User-Agent': 'Request-Promise'
+    },
     json: true // Automatically parses the JSON string in the response
 };
 
 rp(options)
     .then(repos => {
-        console.log('User has %d repos', repos_url.length);
+        console.log(repos.length);
+        var num = repos[0].contributors_url;
+        console.log(num);
     })
     .catch(err => {
+        console.log('Error happened');
         console.log(err);
     });
-
-// var getAuthor = (author, callback) => {
-//     request(
-//         {
-//             url: `https://api.github.com/users/${author}`,
-//             json: true
-//         },
-//         (error, response, body) => {
-//             if (!error && response.statusCode === 200) {
-//                 callback(undefined, {
-//                     repos: body.repos_url
-//                 });
-//             } else if (error) {
-//                 callback('unable to connect to api.github');
-//             } else {
-//                 callback('Unable to fetch Author');
-//             }
-//         }
-//     );
-// };
-
-module.exports = {
-    getAuthor
-};
