ceb04ed52602a946774838d0eb181f48506a083b
Arpit <rpittrer@gmail.com>
2018-09-12 17:24:13 +0530

    handles_filter

--- Authors/getAuthor.js
+++ Authors/getAuthor.js
@@ -1,8 +1,7 @@
-const rp = require('request-promise');
+const request = require('request-promise');
 const data = require('../Data/results.json');
 
-console.log(data[0].git1);
-
+// Schema for Storage
 let Team = {
     teamName: '',
     members: [
@@ -25,41 +24,41 @@ getAuthor = data => {
             registration.git4,
             registration.git5
         ];
-        handles.map(handle => handle.slice(19, handle.length));
-    });
 
-    let options = {
-        uri: `https://api.github.com/users/${handle}/repos`,
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json: true // Automatically parses the JSON string in the response
-    };
+        handles = handles.filter(handle => handle.length > 0);
+        handles = handles.map(handle => handle.slice(19, handle.length));
 
-    rp(options)
-        .then(repos => {
-            Author.numberOfRepos = repos.length;
+        handles.forEach(handle => {
+            let options = {
+                uri: `https://api.github.com/users/${handle}/repos`,
+                headers: {
+                    'User-Agent': 'Request-Promise'
+                },
+                json: true
+            };
+            request(options)
+                .then(repos => {
+                    let newOptions = options;
+                    newOptions.uri = repos[7].contributors_url;
+                    return newOptions;
+                })
+                .then(newOptions => {
+                    let contributor = request(newOptions);
+                    return contributor;
+                })
+                .then(contributor => {
+                    let total = 0;
 
-            let newOptions = options;
-            newOptions.uri = repos[7].contributors_url;
-            return newOptions;
-        })
-        .then(newOptions => {
-            let contributor = rp(newOptions);
-            return contributor;
-        })
-        .then(contributor => {
-            let total = 0;
+                    contributor.forEach(item => {
+                        total += item.contributions;
+                    });
 
-            contributor.forEach(item => {
-                total += item.contributions;
-            });
-
-            Author.numberOfCommits = total;
-            console.log(Author);
-        })
-        .catch(err => {
-            console.log('Error Happened');
-            console.log(err);
+                    Author.numberOfCommits = total;
+                })
+                .catch(err => {
+                    console.log('Error Happened');
+                    console.log(err);
+                });
         });
+    });
 };
