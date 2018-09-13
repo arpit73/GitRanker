6e52cd830454412fd747f876a0f5ddfa5ffdfdd3
Arpit <rpittrer@gmail.com>
2018-09-13 12:09:10 +0530

    data extracted

--- Authors/scraping.js
+++ Authors/scraping.js
@@ -2,7 +2,7 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-const data = require('../Data/results_test.json');
+const data = require('../Data/results.json');
 
 getAuthor = data => {
     let count = 0;
@@ -22,21 +22,13 @@ getAuthor = data => {
             registration.git4,
             registration.git5
         ];
-        let emails = [
-            registration['Email Id of 1st member'],
-            registration['Email Id of 2nd member'],
-            registration['Email Id of 3rd member'],
-            registration['Email Id of 4th member'],
-            registration['Email Id of 4th member']
-        ];
-        
+        Team.email = registration['Email Address'];
+
         //emails = emails.filter(email => email.length > 0);
         handles = handles.filter(handle => handle.length > 0);
 
-        Team.email = emails[count];
-        count++;
-        
         handles.forEach(handle => {
+           
             let options = {
                 uri: `${handle}`,
                 headers: {
@@ -50,7 +42,7 @@ getAuthor = data => {
                     var numbers = element.match(/\d+/g).map(Number);
                     Team.commits = numbers[0];
 
-                    console.log(Team);
+                   //console.log(Team);
                     fs.appendFileSync(
                         'data.json',
                         JSON.stringify(Team, null, 2) + ',',
@@ -62,7 +54,6 @@ getAuthor = data => {
                 });
         });
     });
-   
 };
 
 getAuthor(data);
