004ad6520e091291d2d4c83a4159eca022ce7dcb
Arpit <rpittrer@gmail.com>
2018-09-13 12:25:32 +0530

    selections sorted

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const data = require("./data.json");
+const data = require('./data.json');
+const fs = require('fs');
 
 console.log(data.length);
+
+let selections = data.sort((a, b) => {
+    let commitA = a.commits,
+        commitB = b.commits;
+    return commitB - commitA; //sort by date ascending
+});
+
+//selections = selections.slice(0, 36);
+
+fs.appendFileSync(
+    'selections.json',
+    JSON.stringify(selections, null, 2),
+    'utf-8'
+);
